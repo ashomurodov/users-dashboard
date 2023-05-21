ebaf1a4bbd3f9302dbe50e554090860394cbce4d
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-21 14:11:57 +0500

    users-table-first-commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+const usersFullNameEl = document.querySelectorAll(".name");
+const usersUserNameEl = document.querySelectorAll(".username");
+const usersEmailEl = document.querySelectorAll(".email");
+
+const users: any[] = [];
+const request = fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    users.push(data);
+    drawUser(users);
+  });
+
+// console.log(request);
+
+function drawUser([users]: any[]) {
+  console.log(users);
+  for (let i = 0; i < users.length; i++) {
+    usersFullNameEl[i].textContent = users[i].name;
+    usersUserNameEl[i].textContent = users[i].username;
+    usersEmailEl[i].textContent = users[i].email;
+  }
+}
