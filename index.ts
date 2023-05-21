eb33943ab72b0fced52100656b8dff4e758e207d
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-21 15:07:22 +0500

    delete user

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 const usersFullNameEl = document.querySelectorAll(".name");
 const usersUserNameEl = document.querySelectorAll(".username");
 const usersEmailEl = document.querySelectorAll(".email");
+const usersAddressEl = document.querySelectorAll(".address");
+const userDeleteEl = document.querySelectorAll(".delete");
+const tableEl = document.querySelectorAll(".tableElement");
+const usersIdEl = document.querySelectorAll(".id");
 
 const users: any[] = [];
 const request = fetch("https://jsonplaceholder.typicode.com/users")
@@ -20,5 +24,23 @@ function drawUser([users]: any[]) {
     usersFullNameEl[i].textContent = users[i].name;
     usersUserNameEl[i].textContent = users[i].username;
     usersEmailEl[i].textContent = users[i].email;
+    usersAddressEl[i].textContent = users[i].address.city;
+  }
+}
+
+userDeleteEl.forEach((deleteEl, idx) => {
+  deleteEl.addEventListener("click", (e) => {
+    e.stopPropagation();
+    tableEl[idx].classList.add("none");
+    usersIdEl[idx].classList.remove("id");
+    editTableId();
+  });
+});
+
+function editTableId() {
+  let number = 1;
+  const tableIdEl = document.querySelectorAll(".id");
+  for (let id of tableIdEl) {
+    id.textContent = `${number++}`;
   }
 }
